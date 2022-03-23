8ae550c73c9e14ed52c3d8e2cf18204fc0a68f6e
Avrahm <avrahmk@me.com>
2022-03-23 14:13:55 -0400

    test oauth connection

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const webpack = require('webpack')
+
+const { parsed: myEnv } = require('dotenv').config({
+  path: '.env',
+})
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
+    return config
+  }
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import SignUp from '../components/signup'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -14,6 +15,8 @@ const Home: NextPage = () => {
       <h1 className="text-3xl font-bold underline">
         Welcome to DD Twitter List
       </h1>
+
+      <SignUp />
 
     </div>
   )
