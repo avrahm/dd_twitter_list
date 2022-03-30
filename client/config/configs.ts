2eaac2a39892456318d4305044a92943eb1cc4c8
Avrahm <avrahmk@me.com>
2022-03-30 07:22:05 -0400

    updated configs NEXT_PUBLIC_

--- client/config/configs.ts
+++ client/config/configs.ts
@@ -1,9 +1,9 @@
 export const configs = {
-    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL as string,
+    FIREBASE_DATABASE_URL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL as string,
     TWITTER_CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY as string,
     TWITTER_CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET as string,
-    TWITTER_BEARER_TOKEN: process.env.TWITTER_BEARER_TOKEN as string,
-    TWITTER_CALLBACK_URL: process.env.TWITTER_CALLBACK_URL as string,
+    TWITTER_BEARER_TOKEN: process.env.NEXT_PUBLIC_TWITTER_BEARER_TOKEN as string,
+    TWITTER_CALLBACK_URL: process.env.NEXT_PUBLIC_TWITTER_CALLBACK_URL as string,
     TWITTER_REQUEST_TOKEN_URL: "https://api.twitter.com/oauth/request_token",
     TWITTER_ACCESS_TOKEN_URL: "https://api.twitter.com/oauth/access_token",
     TWITTER_AUTHORIZE_URL: "https://api.twitter.com/oauth/authorize",
@@ -11,15 +11,15 @@ export const configs = {
     TWITTER_API_VERSION: "",
     TWITTER_OWNER_TOKEN: process.env.TWITTER_LIST_OWNER_TOKEN as string,
     TWITTER_OWNER_SECRET: process.env.TWITTER_LIST_OWNER_SECRET as string,
-    API_URL: process.env.FIREBASE_API_URL as string,
-    TWITTER_DD_LIST_ID: process.env.TWITTER_DD_LIST_ID as string,
+    API_URL: process.env.NEXT_PUBLIC_FIREBASE_API_URL as string,
+    TWITTER_DD_LIST_ID: process.env.NEXT_PUBLIC_TWITTER_DD_LIST_ID as string,
 };
 
 export const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
