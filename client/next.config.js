f96b05fb713b81ce698892f978bd804ccafcdc56
Avrahm <avrahmk@me.com>
2022-03-30 07:16:04 -0400

    comment out webpack

--- client/next.config.js
+++ client/next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
-const webpack = require("webpack");
+// const webpack = require("webpack");
 
-const { parsed: myEnv } = require("dotenv").config({
-    path: ".env.local", // to resolve empty if .env file not found
-    debug: true,
-});
+// const { parsed: myEnv } = require("dotenv").config({
+//     path: ".env.local", // to resolve empty if .env file not found
+//     debug: true,
+// });
 
 const nextConfig = {
     reactStrictMode: true,
-    webpack(config) {
-        if (process.env.NODE_ENV === "development") {
-            config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
-            return config;
-        }
-    },
+    // webpack(config) {
+    //     if (process.env.NODE_ENV === "development") {
+    //         config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    //         return config;
+    //     }
+    // },
 
     images: {
         domains: ["pbs.twimg.com"],
